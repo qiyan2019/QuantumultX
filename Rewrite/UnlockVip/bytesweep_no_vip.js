3447b4989d5b69833824c1659cd9bd2b3e74180e
qiyan2019 <1304195+qiyan2019@users.noreply.github.com>
2025-09-12 16:15:18 +0800

    Update bytesweep_no_vip.js

--- Rewrite/UnlockVip/bytesweep_no_vip.js
+++ Rewrite/UnlockVip/bytesweep_no_vip.js
@@ -1,12 +1,8 @@
 // QuantumultX - 修改 Byte Sweep 会员状态为非会员
 var body = $response.body
     // 将 bytesweepNewvip 改为 0
-    .replace(/"bytesweepNewvip"\s*:\s*\d+/g, '"bytesweepNewvip":0')
+    .replace(/"bytesweepNewvip"\s*:\s*\d+/g, '"bytesweepNewvip":1')
     // 将 bytesweepVip 改为 0
-    .replace(/"bytesweepVip"\s*:\s*\d+/g, '"bytesweepVip":0')
-    // 清空 bytesweepNewvipCt
-    .replace(/"bytesweepNewvipCt"\s*:\s*"\[.*?\]"/g, '"bytesweepNewvipCt":"[]"')
-    // 清空 bytesweepVipCt
-    .replace(/"bytesweepVipCt"\s*:\s*"\[.*?\]"/g, '"bytesweepVipCt":"[]"');
+    .replace(/"bytesweepVip"\s*:\s*\d+/g, '"bytesweepVip":2')
 
 $done({ body });
